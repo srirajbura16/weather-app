a6ff21a31293db5bbe46a749b2662ff86133858c
Sriraj Bura <sriraj.bura16@gmail.com>
2021-05-14 04:51:55 -0400

    refactored async code

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ import displayDOM from './displayDOM';
 document.querySelector('form').addEventListener('submit', e => {
   e.preventDefault();
   const city = document.querySelector('.city-input').value;
-  getWeatherData(city)
-    .then(weatherData => {
-      console.log(weatherData);
-      return processData(weatherData);
-    })
-    .then(data => {
-      displayDOM(data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+
+  showWeather(city);
+
+  e.target.reset();
 });
+
+async function showWeather(city) {
+  const weatherData = await getWeatherData(city);
+  const data = processData(weatherData);
+  const displayData = displayDOM(data);
+}
